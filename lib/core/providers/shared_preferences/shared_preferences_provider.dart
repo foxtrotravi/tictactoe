89bd4a4bd280d4e4f188ba34f557a8fea864d531
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2023-04-08 17:39:44 +0530

    Create SharedPreferences provider

---
+++ lib/core/providers/shared_preferences/shared_preferences_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPreferences = FutureProvider<SharedPreferences>(
+  (ref) async => await SharedPreferences.getInstance(),
+);
