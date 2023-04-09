d43c38f3a21b931f5e0fdade1d7b5905194b6da0
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2023-04-09 19:55:38 +0530

    Handle navigation for existing users

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tictactoe/core/constants/constants.dart';
+import 'package:tictactoe/core/providers/index.dart';
+import 'package:tictactoe/core/routing/routes.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends ConsumerWidget {
   const OnboardingPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: const Text('Onboarding'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: const Text('Onboarding'),
+            ),
+            ElevatedButton(
+              onPressed: () => onPressed(context, ref),
+              child: const Text('Home page'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> onPressed(BuildContext context, WidgetRef ref) async {
+    context.goNamed(Routes.home);
+    await ref.read(sharedPreferencesProvider).value!.setBool(kIsNewUser, false);
   }
 }
